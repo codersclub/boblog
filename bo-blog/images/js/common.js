92360677841920edc4c8a73ffb363a729e062004
Valery Votintsev <vvotintsev@gmail.com>
2009-03-27 23:21:14 +0300

    Apply the HotFix 20090327

--- bo-blog/images/js/common.js
+++ bo-blog/images/js/common.js
@@ -5,6 +5,7 @@ var is_saf    = ((userAgent.indexOf('applewebkit') != -1) || (navigator.vendor =
 var is_webtv  = (userAgent.indexOf('webtv') != -1);
 var is_ie     = ((userAgent.indexOf('msie') != -1) && (!is_opera) && (!is_saf) && (!is_webtv));
 var is_ie4    = ((is_ie) && (userAgent.indexOf('msie 4.') != -1));
+var is_ie8    = ((is_ie) && (userAgent.indexOf('msie 8.') != -1));
 var is_moz    = ((navigator.product == 'Gecko') && (!is_saf));
 var is_kon    = (userAgent.indexOf('konqueror') != -1);
 var is_ns     = ((userAgent.indexOf('compatible') == -1) && (userAgent.indexOf('mozilla') != -1) && (!is_opera) && (!is_webtv) && (!is_saf));
