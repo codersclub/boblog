5366de424a230c426a67bc89cb0c08f7bfc889a3
Valery Votintsev <vvotintsev@gmail.com>
2010-02-23 11:17:48 +0300

    Add "ColdNight" template
    --HG--
    branch : templates

---
+++ bo-blog/template/ColdNight/info.php
@@ -0,0 +1,12 @@
+<?PHP
+$template['id']="ColdNight";
+$template['sysver']='1.0';
+$template['name']="ColdNight for bo-blog2.1.1";
+$template['author']="http://www.tripmind.com/";
+$template['intro']="ColdNight skin of Bo-blog 2.1.0.";
+$template['dirname']="ColdNight";
+$template['thumbnail']="thumb.jpg";
+$template['structure']="template/ColdNight/elements.php";
+$template['images']="template/ColdNight/images";
+$template['moreimages']="template/ColdNight/images";
+$template['css'][0]="template/ColdNight/styles.css";
