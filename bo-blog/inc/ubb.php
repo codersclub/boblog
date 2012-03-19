c8242d4ee33d6b8b8438483f556565200d4cd736
Valery Votintsev <vvotintsev@gmail.com>
2012-03-19 17:31:27 +0300

    Use preg_replace_callback instead of /ie

--- bo-blog/inc/ubb.php
+++ bo-blog/inc/ubb.php
@@ -12,8 +12,28 @@ In memory of my university life
 
 if (!defined('VALIDREQUEST')) die ('Access Denied.');
 
+/*vot*/ function inrss($inrss=null) {
+/*vot*/	static $RSS = 0;
+/*vot*/	if(!is_null($inrss)) {
+/*vot*/		$RSS = $inrss;
+/*vot*/	}
+/*vot*/	return $RSS;
+/*vot*/}
+
+/*vot*/ function advanced($advanced=null) {
+/*vot*/	static $ADV = 0;
+/*vot*/	if(!is_null($advanced)) {
+/*vot*/		$ADV = $advanced;
+/*vot*/	}
+/*vot*/	return $ADV;
+/*vot*/}
+
 function convert_ubb ($str, $advanced=0, $inrss=0) {
 	global $logstat, $openidloginstat, $template, $mbcon, $lnc, $config;
+
+/*vot*/	inrss($inrss);
+/*vot*/	advanced($advanced);
+
 	if ($logstat!=1) {
 		$str=preg_replace("/\[hide\](.+?)\[\/hide\]/is", "<div class=\"quote hidebox\"><div class=\"quote-title\">{$lnc[311]}</div><div class=\"quote-content\">{$lnc[312]}  <a href=\"{$config['blogurl']}/login.php?job=register\">{$lnc[79]}</a> {$lnc[235]} <a href=\"{$config['blogurl']}/login.php\">{$lnc[89]}</a> </div></div>", $str);
 	} else {
@@ -27,30 +47,25 @@ function convert_ubb ($str, $advanced=0, $inrss=0) {
 
 	//[IMG]
 	if ($advanced==1) {
-		$str=preg_replace("/\[url=([^\[]*)\]\[img( align=L| align=M| align=R)?( width=[0-9]+)?( height=[0-9]+)?\]\s*(\S+?)\s*\[\/img\]\[\/url\]/ise","makeimgwithurl('\\1', '\\2', '\\3', '\\4', '\\5', {$inrss})",$str);
-		$str=preg_replace("/\[img( align=L| align=M| align=R)?( width=[0-9]+)?( height=[0-9]+)?\]\s*(\S+?)\s*\[\/img\]/ise","makeimg('\\1', '\\2', '\\3', '\\4', {$inrss})",$str);
+/*vot*/		$str=preg_replace_callback("/\[url=([^\[]*)\]\[img( align=L| align=M| align=R)?( width=[0-9]+)?( height=[0-9]+)?\]\s*(\S+?)\s*\[\/img\]\[\/url\]/is","makeimgwithurl",$str);
+/*vot*/		$str=preg_replace_callback("/\[img( align=L| align=M| align=R)?( width=[0-9]+)?( height=[0-9]+)?\]\s*(\S+?)\s*\[\/img\]/is","makeimg",$str);
 	} else {
-		$str=preg_replace_callback("/\[img( align=L| align=M| align=R)?( width=[0-9]+)?( height=[0-9]+)?\]\s*(\S+?)\s*\[\/img\]/is","makeimginrss",$str);
+/*vot*/		$str=preg_replace_callback("/\[img( align=L| align=M| align=R)?( width=[0-9]+)?( height=[0-9]+)?\]\s*(\S+?)\s*\[\/img\]/is","makeimginrss",$str);
 	}
 
-	if ($mbcon['countdownload']=='1' && $inrss==0) $str=preg_replace(array("/\[sfile\]\s*\[attach\]([0-9]+)\[\/attach\]\s*\[\/sfile\]/ise", "/\[file\]\s*\[attach\]([0-9]+)\[\/attach\]\s*\[\/file\]/ise"), array("makedownload('\\1', 1, 0, true)", "makedownload('\\1', 0, 0, true)"), $str);
+/*vot*/	if ($mbcon['countdownload']=='1' && inrss()==0) {
+/*vot*/		$str=preg_replace_callback("/\[(s)*file\]\s*\[attach\]([0-9]+)\[\/attach\]\s*\[\/(s)*file\]/is", "makedownatt", $str);
+/*vot*/	}
 	$str=preg_replace("/\[attach\]([0-9]+)\[\/attach\]/is", "attachment.php?fid=\\1", $str);
-	$str=preg_replace("/\[sfile\]\s*(\S+?)\s*\[\/sfile\]/ise", "makedownload('\\1', 1, $inrss, false)", $str);
-	$str=preg_replace("/\[file\]\s*(\S+?)\s*\[\/file\]/ise", "makedownload('\\1', 0, $inrss, false)", $str);
+/*vot*/	$str=preg_replace_callback("/\[(s)*file\]\s*(\S+?)\s*\[\/(s)*file\]/is", "makedown", $str);
 
 	//Auto add url link
 	if ($mbcon['autoaddlink']==1) $str=preg_replace("/(?<=[^\]a-z0-9-=\"'\\/])((https?|ftp|gopher|news|telnet|rtsp|mms|callto|ed2k):\/\/|www\.)([a-z0-9\/\-_+=.~!%@?#%&;:$\\()|]+)/i", "[autourl]\\1\\3[/autourl]", $str);
 
 
-	$regubb_search = array(
-				"/\[size=([^\[\<]+?)\](.+?)\[\/size\]/ie",
-				"/\[tbl( width=[0-9]+)?(%)?( bgcolor=[^ ]*)?( border=[^ ]*)?\](.+?)\[\/tbl\]/ise",
+/*vot*/	$regubb_search = array(
 				"/\s*\[quote\][\n\r]*(.+?)[\n\r]*\[\/quote\]\s*/is",
 				"/\s*\[quote=(.+?)\][\n\r]*(.+?)[\n\r]*\[\/quote\]\s*/is",
-				"/\s*\[code\][\n\r]*(.+?)[\n\r]*\[\/code\]\s*/ie",
-				"/\[autourl\]([^\[]*)\[\/autourl\]/ie",
-				"/\[url\]([^\[]*)\[\/url\]/ie",
-				"/\[url=([^\[]*)\](.+?)\[\/url\]/ie",
 				"/\[email\]([^\[]*)\[\/email\]/is",
 				"/\[acronym=([^\[]*)\](.+?)\[\/acronym\]/is",
 				"/\[color=([a-zA-Z0-9#]+?)\](.+?)\[\/color\]/i",
@@ -62,17 +77,10 @@ function convert_ubb ($str, $advanced=0, $inrss=0) {
 				"/\[strike\](.+?)\[\/strike\]/i",
 				"/\[sup\](.+?)\[\/sup\]/i",
 				"/\[sub\](.+?)\[\/sub\]/i",
-				"/\s*\[php\][\n\r]*(.+?)[\n\r]*\[\/php\]\s*/ie"
 	);
-	$regubb_replace =  array(
-				"makefontsize('\\1', '\\2')",
-				"maketable('\\5','\\1','\\2','\\3','\\4')",
+/*vot*/	$regubb_replace =  array(
 				"<div class=\"quote\"><div class=\"quote-title\">{$lnc[265]}</div><div class=\"quote-content\">\\1</div></div>",
 				"<div class=\"quote\"><div class=\"quote-title\">{$lnc[266]} \\1</div><div class=\"quote-content\">\\2</div></div>",
-				"makecode('\\1')",
-				"makeurl('\\1', {$advanced})",
-				"makeurl('\\1', {$advanced})",
-				"makeurl('\\1', {$advanced}, '\\2')",
 				"<a href=\"mailto:\\1\">\\1</a>",
 				"<acronym title=\"\\1\">\\2</acronym>",
 				"<span style=\"color: \\1;\">\\2</span>",
@@ -84,22 +92,33 @@ function convert_ubb ($str, $advanced=0, $inrss=0) {
 				"<del>\\1</del>",
 				"<sup>\\1</sup>",
 				"<sub>\\1</sub>",
-				"xhtmlHighlightString('\\1')"
 	);
 	$str=preg_replace($regubb_search, $regubb_replace, $str);
 
+/*vot*/	$str=preg_replace_callback("/\[size=([^\[\<]+?)\](.+?)\[\/size\]/i", "makefontsize", $str);
+/*vot*/	$str=preg_replace_callback("/\[tbl( width=[0-9]+)?(%)?( bgcolor=[^ ]*)?( border=[^ ]*)?\](.+?)\[\/tbl\]/is", "maketable", $str);
+/*vot*/	$str=preg_replace_callback("/\s*\[code\][\n\r]*(.+?)[\n\r]*\[\/code\]\s*/i", "makecode", $str);
+/*vot*/	$str=preg_replace_callback("/\[autourl\]([^\[]*)\[\/autourl\]/i", "makeurl", $str);
+/*vot*/	$str=preg_replace_callback("/\[url\]([^\[]*)\[\/url\]/i", "makeurl", $str);
+/*vot*/	$str=preg_replace_callback("/\[url=([^\[]*)\](.+?)\[\/url\]/i", "makeurl", $str);
+/*vot*/	$str=preg_replace_callback("/\s*\[php\][\n\r]*(.+?)[\n\r]*\[\/php\]\s*/i", "xhtmlHighlightString", $str);
+
 	//Multimedia Objects, dangerous, so visitors shall never be allowed to post such an object directly
 	if ($advanced==1) {
-		$str =($inrss==0) ?  preg_replace("/\[(wmp|swf|real|flv)=([^\[\<]+?),([^\[\<]+?)\]\s*([^\[\<\r\n]+?)\s*\[\/(wmp|swf|real|flv)\]/ies", "makemedia('\\1', '\\4', '\\2', '\\3')", $str) : preg_replace("/\[(wmp|swf|real|flv)=([^\[\<]+?),([^\[\<]+?)\]\s*([^\[\<\r\n]+?)\s*\[\/(wmp|swf|real|flv)\]/is", "<br/>{$lnc[267]}<br/>", $str);
+/*vot*/		$str =(inrss()==0) ?  preg_replace("/\[(wmp|swf|real|flv)=([^\[\<]+?),([^\[\<]+?)\]\s*([^\[\<\r\n]+?)\s*\[\/(wmp|swf|real|flv)\]/is", "makemedia", $str) : preg_replace("/\[(wmp|swf|real|flv)=([^\[\<]+?),([^\[\<]+?)\]\s*([^\[\<\r\n]+?)\s*\[\/(wmp|swf|real|flv)\]/is", "<br/>{$lnc[267]}<br/>", $str);
 		$str=plugin_walk('ubbanalyseadvance', $str);
 	}
 	return $str;
 
 }
 
-function makeurl($url, $advanced=0, $linktext='') { //2011/2/20 Force admin quit
+/*vot*/ function makeurl($match) { //2011/2/20 Force admin quit
 	global $mbcon, $config;
-	if ($advanced==0) {
+
+/*vot*/	$url=$match[1];
+/*vot*/	$linktext=@$match[2]; // [url=$url]$linktext[/url]
+
+/*vot*/	if (advanced()==0) {
 		$gotoreallink="{$config['blogurl']}/urlredirect.php?go=".(substr(strtolower($url), 0, 4) == 'www.' ? urlencode("http://$url") : urlencode($url));
 	} else {
 		$gotoreallink=substr(strtolower($url), 0, 4) == 'www.' ? "http://$url" : $url;
@@ -120,14 +139,16 @@ function makeurl($url, $advanced=0, $linktext='') { //2011/2/20 Force admin quit
 	return $urllink;
 }
 
-function makefontsize ($size, $word) {
+/*vot*/function makefontsize ($match) {
+/*vot*/	$size = $match[1];
+/*vot*/	$word = $match[2];
 	$word=stripslashes($word);
 	$sizeitem=array (0, 8, 10, 12, 14, 18, 24, 36);
 	$size=$sizeitem[$size];
 	return "<span style=\"font-size: {$size}px;\">{$word}</span>";
 }
 
-function makemedia ($match) {
+/*vot*/ function makemedia ($match) {
 /*vot*/	$mediatype=$match[1];
 /*vot*/	$url=$match[4];
 /*vot*/	$width=$match[2];
@@ -143,15 +164,20 @@ function makemedia ($match) {
 	return $str;
 }
 
-function makecode ($str) {
+function makecode ($match) {
+/*vot*/	$str = $match[1];
 	$str=str_replace('[autourl]', '', $str);
 	$str=str_replace('[/autourl]', '', $str);
 	return "<div class=\"code\">{$str}</div>";
 }
 
-function makeimg ($aligncode, $widthcode, $heightcode, $src, $inrss=0) {
+/*vot*/ function makeimg ($match) {
 	global $lnc, $mbcon, $config;
 /*vot*/	$show = '';
+/*vot*/ $aligncode  = $match[1];
+/*vot*/ $widthcode  = $match[2];
+/*vot*/ $heightcode = $match[3];
+/*vot*/ $src        = $match[4];
 	$align=str_replace(' align=', '', strtolower($aligncode));
 	if ($align=='l') $show=' align="left"';
 	elseif ($align=='r') $show=' align="right"';
@@ -160,14 +186,19 @@ function makeimg ($aligncode, $widthcode, $heightcode, $src, $inrss=0) {
 	if (!empty($width)) $show.=" width=\"{$width}\"";
 	$height=str_replace(' height=', '', strtolower($heightcode));
 	if (!empty($height)) $show.=" height=\"{$height}\"";
-	if ($inrss==1) $src=(substr(strtolower($src), 0, 4) == 'http') ? $src : $config['blogurl'].'/'.$src;
-	$onloadact=($inrss==0 && !empty($mbcon['autoresizeimg'])) ? " onload=\"if(this.width>{$mbcon['autoresizeimg']}) {this.resized=true; this.width={$mbcon['autoresizeimg']};}\"" : '';
+/*vot*/	if (inrss()==1) $src=(substr(strtolower($src), 0, 4) == 'http') ? $src : $config['blogurl'].'/'.$src;
+/*vot*/	$onloadact=(inrss()==0 && !empty($mbcon['autoresizeimg'])) ? " onload=\"if(this.width>{$mbcon['autoresizeimg']}) {this.resized=true; this.width={$mbcon['autoresizeimg']};}\"" : '';
 	$code="<a href=\"{$src}\" target=\"_blank\"><img src=\"{$src}\" class=\"insertimage\" alt=\"{$lnc[231]}\" title=\"{$lnc[231]}\" border=\"0\"{$onloadact}{$show}/></a>";
 	return $code;
 }
 
-function makeimgwithurl ($url, $aligncode, $widthcode, $heightcode, $src, $inrss=0) {
+/*vot*/ function makeimgwithurl ($match) {
 	global $lnc, $mbcon, $config;
+/*vot*/	$url        = $match[1];
+/*vot*/ $aligncode  = $match[2];
+/*vot*/ $widthcode  = $match[3];
+/*vot*/ $heightcode = $match[4];
+/*vot*/ $src        = $match[5];
 	$align=str_replace(' align=', '', strtolower($aligncode));
 	if ($align=='l') $show=' align="left"';
 	elseif ($align=='r') $show=' align="right"';
@@ -176,14 +207,14 @@ function makeimgwithurl ($url, $aligncode, $widthcode, $heightcode, $src, $inrss
 	if (!empty($width)) $show.=" width=\"{$width}\"";
 	$height=str_replace(' height=', '', strtolower($heightcode));
 	if (!empty($height)) $show.=" height=\"{$height}\"";
-	if ($inrss==1) $src=(substr(strtolower($src), 0, 4) == 'http') ? $src : $config['blogurl'].'/'.$src;
-	$onloadact=($inrss==0 && !empty($mbcon['autoresizeimg'])) ? " onload=\"if(this.width>{$mbcon['autoresizeimg']}) {this.resized=true; this.width={$mbcon['autoresizeimg']};}\"" : '';
+/*vot*/	if (inrss()==1) $src=(substr(strtolower($src), 0, 4) == 'http') ? $src : $config['blogurl'].'/'.$src;
+/*vot*/	$onloadact=(inrss()==0 && !empty($mbcon['autoresizeimg'])) ? " onload=\"if(this.width>{$mbcon['autoresizeimg']}) {this.resized=true; this.width={$mbcon['autoresizeimg']};}\"" : '';
 	$code="<a href=\"{$url}\" target=\"_blank\"><img src=\"{$src}\" class=\"insertimage\" alt=\"{$lnc[231]}\" title=\"{$lnc[231]}\" border=\"0\"{$onloadact}{$show}/></a>";
 	return $code;
 }
 
 
-function makeimginrss($match) {
+/*vot*/ function makeimginrss($match) {
 /*vot*/	$src = $match[4];
 	global $config, $lnc, $template;
 	$src=(substr(strtolower($src), 0, 4) == 'http') ? $src : $config['blogurl'].'/'.$src;
@@ -191,8 +222,8 @@ function makeimginrss($match) {
 	return $str;
 }
 
-function xhtmlHighlightString($str) {
-	$str=base64_decode($str);
+/*vot*/ function xhtmlHighlightString($watch) {
+/*vot*/	$str=base64_decode($watch[1]);
 	if (PHP_VERSION<'4.2.0') return "<div class=\"code\" style=\"overflow: auto;\">$str</div>";
 	$hlt = highlight_string($str, true);
 	if (PHP_VERSION>'5') return "<div class=\"code\" style=\"overflow: auto;\">$hlt</div>";
@@ -201,7 +232,19 @@ function xhtmlHighlightString($str) {
 	return "<div class=\"code\" style=\"overflow: auto;\">$ret</div>";
 }
 
-function makedownload ($url, $sfile, $inrss, $isattached=false) {
+/*vot*/ function makedownatt ($match) {
+/*vot*/	$sfile = intval(($match[1] == 's')); // $match[1] = (s)*file
+/*vot*/	$url = $match[2];
+	return makedownload ($url, $sfile, true);
+}
+
+/*vot*/ function makedown ($match) {
+/*vot*/	$sfile = intval(($match[1] == 's')); // $match[1] = (s)*file
+/*vot*/	$url = $match[2];
+/*vot*/	return makedownload ($url, $sfile, false);
+/*vot*/}
+
+/*vot*/function makedownload ($url, $sfile, $isattached=false) {
 	global $logstat, $openidloginstat, $template, $lnc, $mbcon, $dlstat, $config;
 	if ($isattached) {
 		$downloadtime=" ({$lnc[280]} <!--global:{dlstat_{$url}}--> {$lnc[281]})";
@@ -209,7 +252,7 @@ function makedownload ($url, $sfile, $inrss, $isattached=false) {
 		$dlstat[]=$url;
 		$url="attachment.php?fid={$url}";
 	}
-	if ($inrss==0) {
+/*vot*/	if (inrss()==0) {
 		if (($logstat==1 || $openidloginstat==1) || $sfile!=1) $str="<div class=\"quote downloadbox\"><div class=\"quote-title\"><img src=\"{$template['images']}/download.gif\" alt=\"\"/>{$lnc[232]} {$downloadtime}</div><div class=\"quote-content\"><a href=\"{$url}\">{$lnc[233]}{$downloadtime2}</a></div></div>";
 		else  $str="<div class=\"quote\"><div class=\"quote-title\"><img src=\"{$template['images']}/download.gif\" alt=\"\"/>{$lnc[232]}{$downloadtime}</div><div class=\"quote-content\">{$lnc[234]} <a href=\"{$config['blogurl']}/login.php?job=register\">{$lnc[79]}</a> {$lnc[235]} <a href=\"{$config['blogurl']}/login.php\">{$lnc[89]}</a> </div></div>";
 	} else {
@@ -219,7 +262,12 @@ function makedownload ($url, $sfile, $inrss, $isattached=false) {
 	return $str;
 }
 
-function maketable ($tablebody, $widthcode, $ifpercentage, $bgcolorcode, $bordercolorcode) {
+/*vot*/ function maketable ($match) {
+/*vot*/	$tablebody = $match[5];
+/*vot*/	$widthcode = $match[1];
+/*vot*/	$ifpercentage = $match[2];
+/*vot*/	$bgcolorcode = $match[3];
+/*vot*/	$bordercolorcode = $match[4];
 	$tablebody=stripslashes($tablebody);
 	$show="<table";
 	$width=str_replace(' width=', '', strtolower($widthcode));
