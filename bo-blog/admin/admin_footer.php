6ba03fce4b7afb65a4c03543b040428bfc8f396a
Valery Votintsev <vvotintsev@gmail.com>
2012-03-23 21:51:03 +0300

    Add debug info: SQL Query list

--- bo-blog/admin/admin_footer.php
+++ bo-blog/admin/admin_footer.php
@@ -13,7 +13,22 @@ $display_overall.="
     [<a href=\"login.php?job=logout\">{$lna[40]}</a>]
   </div>
 </div>
+";
 
-</body></html>";
+if (defined('VALIDADMIN') && $adminlogstat) {
+  if($config['debug']=='1') {
+    $display_overall.="DEBUG:<br><table border=1 cellpadding=4 width='100%'>\n";
+    $display_overall.="<tr><th>#</th><th>SQL</th><th>Err.code</th><th>Err.Msg</th></tr>\n";
+
+    foreach($allqueries as $i=>$query) {
+    	$display_overall.="<tr><td>{$i}</td><td align=\"left\">{$query['sql']}</td><td>{$query['err_code']}</td><td>{$query['err_message']}</td></tr>\n";
+    }
+
+    $display_overall.="</table>\n<br>\n";
+  }
+}
+
+$display_overall.="</body></html>";
+
 @header("Content-Type: text/html; charset=utf-8");
 echo ($display_overall);
