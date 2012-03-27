b5d55245924821bd547d5b35b3b567ba7f84a3e1
Valery Votintsev <vvotintsev@gmail.com>
2012-03-27 23:16:18 +0300

    Add "hislide" plugin

---
+++ bo-blog/plugin/highslide/firstheader.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('VALIDREQUEST')) die ('Access Denied.');
+function plugin_highslide_firstheader ($str) {
+$str.=<<<eot
+<link rel="stylesheet" type="text/css" href="plugin/highslide/highslide.css" />
+<script type="text/javascript" src="plugin/highslide/highslide-with-gallery.js"></script>
+<script type="text/javascript">
+	hs.graphicsDir = 'plugin/highslide/graphics/';
+hs.align = 'center';
+hs.transitions = ['expand', 'crossfade'];
+hs.outlineType = 'rounded-white';
+hs.fadeInOut = true;
+hs.numberPosition = 'caption';
+hs.dimmingOpacity = 0.75;
+
+// Add the controlbar
+if (hs.addSlideshow) hs.addSlideshow({
+	//slideshowGroup: 'group1',
+	interval: 5000,
+	repeat: false,
+	useControls: true,
+	fixedControls: 'fit',
+	overlayOptions: {
+		opacity: .75,
+		position: 'bottom center',
+		hideOnMouseOut: true
+		}
+	});
+</script>
+eot;
+	return $str;
+}
