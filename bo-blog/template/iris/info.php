d8525051d93e28761362b06d2e510bae1c7f3a0b
Valery Votintsev <vvotintsev@gmail.com>
2012-03-29 21:08:45 +0300

    Add new template "iris"

---
+++ bo-blog/template/iris/info.php
@@ -0,0 +1,12 @@
+<?PHP
+$template['id']="iris080202";
+$template['sysver']='5.2';
+$template['name']="Iris";
+$template['author']="Picturepan2";
+$template['intro']="Transplant by 404cn";
+$template['dirname']="iris";
+$template['thumbnail']="screenshot.png";
+$template['structure']="template/iris/elements.php";
+$template['images']="template/iris/images";
+$template['moreimages']="template/iris/images";
+$template['css'][0]="template/iris/styles.css";
