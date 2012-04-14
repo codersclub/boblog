36f23d6e3d24c4e012c50c181ab2b696d9b49b2f
Valery Votintsev <vvotintsev@gmail.com>
2012-04-14 21:41:53 +0300

    Add Mysqli support

--- bo-blog/inc/db.php
+++ bo-blog/inc/db.php
@@ -16,46 +16,80 @@ $persistant_connect=0;
 //If you don't want to discontinue the program when there is any database query error, set the following variety as  1
 $ignore_db_errors=0;
 
+global $dbh, $db_version;
+$dbh = false;
+$db_version = '';
+$db_connected = false;
+
 if (!defined('VALIDREQUEST')) die ('Access Denied.');
 
-if (!function_exists("mysql_connect")) {
+if (!function_exists("mysql_connect") &&
+    !function_exists("mysqli_connect")) {
 	die ("Your server does not seem to support MySQL, so Bo-Blog 2.x can not run at your server.");
 }
 
 function db_connect($dbhost, $dbuser, $dbpw, $dbname='') {
+	global $db_type, $dbh, $db_version;
 	global $db_410, $db_connected, $persistant_connect;
+
 	if ($db_connected==1) return;
+
 	if ($persistant_connect==1) {
-/*vot*/		$dbh=@mysql_pconnect($dbhost, $dbuser, $dbpw);
+		if($db_type == 'mysqli') {
+			$dbh = @mysqli_pconnect($dbhost, $dbuser, $dbpw);
+		} else {
+			$dbh = @mysql_pconnect($dbhost, $dbuser, $dbpw);
+		}
 	} else {
-/*vot*/		$dbh=@mysql_connect($dbhost, $dbuser, $dbpw);
+		if($db_type == 'mysqli') {
+			$dbh = @mysqli_connect($dbhost, $dbuser, $dbpw);
+		} else {
+			$dbh = @mysql_connect($dbhost, $dbuser, $dbpw);
+		}
 	}
 
-/*vot*/	if(!$dbh) {
-/*vot*/		db_halt('Can not connect to MySQL server');
-/*vot*/	}
+	if(!$dbh) {
+		db_halt('Can not connect to MySQL server');
+	}
 
 	$db_connected=1;
+
+	if($db_type == 'mysqli') {
+		$db_version = mysqli_get_server_info($dbh);
+	} else {
+		$db_version = mysql_get_server_info();
+	}
 
 	if ($db_410=='1') {
 		db_query("SET NAMES 'utf8'");
 	}
 
 	if (!empty($dbname)) {
-		$a_result=mysql_select_db($dbname);
+		if($db_type == 'mysqli') {
+			$a_result=mysqli_select_db($dbh, $dbname);
+		} else {
+			$a_result=mysql_select_db($dbname);
+		}
 		if ($a_result) {
 			return $a_result;
 		} else {
-/*vot*/			db_halt('Can not connect to MySQL server');
+			db_halt('Can not connect to MySQL server');
 		}
 	}
 }
 
 /*vot
 function db_select_db($dbname) {
-	$a_result=mysql_select_db($dbname);
+	global $db_type, $dbh;
+	if($db_type == 'mysqli') {
+		$a_result=mysqli_select_db($dbname);
+	} else {
+		$a_result=mysql_select_db($dbname);
+	}
 	if ($a_result) {
-		if (mysql_get_server_info()>='4.1.0') {
+		if($db_type == 'mysqli') {
+			mysqli_query($dbh, "SET NAMES 'utf8'");
+		} else {
 			mysql_query("SET NAMES 'utf8'");
 		}
 	}
@@ -64,28 +98,44 @@ function db_select_db($dbname) {
 */
 
 function db_fetch_array($query, $result_type = MYSQL_ASSOC) {
-/*vot*/	$res = mysql_fetch_array($query, $result_type);
-/*vot*/	return $res;
+	global $db_type;
+	if($db_type == 'mysqli') {
+		return mysqli_fetch_array($query, $result_type);
+	} else {
+		return mysql_fetch_array($query, $result_type);
+	}
 }
 
 function db_query($sql, $silence = 0) {
+	global $db_type, $dbh;
 	global $querynum, $allqueries, $ignore_db_errors;
-	$query = mysql_query($sql);
-	if(!$query && !$silence && !$ignore_db_errors) {
-		db_halt('MySQL Query Error', $sql);
+	if($db_type == 'mysqli') {
+		$query = mysqli_query($dbh, $sql);
+	} else {
+		$query = mysql_query($sql);
+	}
+	if($db_type == 'mysqli') {
+		$errno = mysqli_errno($dbh);
+		$errmess = mysqli_error($dbh);
+	} else {
+		$errno = mysql_errno();
+		$errmess = mysql_error();
 	}
 	$querynum++;
 /*vot*/	$allqueries[]=array( //For Debug Use Only
 /*vot*/		'sql' => $sql,
-/*vot*/		'err_code' => mysql_errno(),
-/*vot*/		'err_message' => mysql_error(),
+/*vot*/		'err_code' => $errno,
+/*vot*/		'err_message' => $errmess,
 	);
+	if(!$query && !$silence && !$ignore_db_errors) {
+		db_halt('MySQL Query Error', $sql);
+	}
 	return $query;
 }
 
 function db_unbuffered_query($sql, $silence = 0) {
-	global $querynum;
-	$func_unbuffered_query = @function_exists('mysql_unbuffered_query') ? 'mysql_unbuffered_query' : 'mysql_query';
+	global $querynum, $db_type;
+	$func_unbuffered_query = @function_exists($db_type.'_unbuffered_query') ? $db_type.'_unbuffered_query' : $db_type.'_query';
 	$query = $func_unbuffered_query($sql);
 	if(!$query && !$silence) {
 		db_halt('MySQL Query Error', $sql);
@@ -95,74 +145,142 @@ function db_unbuffered_query($sql, $silence = 0) {
 }
 
 function db_affected_rows() {
-	return mysql_affected_rows();
+	global $db_type, $dbh;
+	if($db_type == 'mysqli') {
+		return mysqli_affected_rows($dbh);
+	} else {
+		return mysql_affected_rows();
+	}
 }
 
 function db_error() {
-	return mysql_error();
+	global $db_type, $dbh;
+	if($db_type == 'mysqli') {
+		return mysqli_error($dbh);
+	} else {
+		return mysql_error();
+	}
 }
 
 function db_errno() {
-	return mysql_errno();
+	global $db_type, $dbh;
+	if($db_type == 'mysqli') {
+		return mysqli_errno($dbh);
+	} else {
+		return mysql_errno();
+	}
 }
 
 function result($query, $row) {
-	$query = @mysql_result($query, $row);
+	global $db_type;
+	if($db_type == 'mysqli') {
+		$query = @mysqli_result($query, $row);
+	} else {
+		$query = @mysql_result($query, $row);
+	}
 	return $query;
 }
 
 function db_num_rows($query) {
-	$query = mysql_num_rows($query);
+	global $db_type;
+	if($db_type == 'mysqli') {
+		$query = mysqli_num_rows($query);
+	} else {
+		$query = mysql_num_rows($query);
+	}
 	return $query;
 }
 
 function db_num_fields($query) {
-	return mysql_num_fields($query);
+	global $db_type;
+	if($db_type == 'mysqli') {
+		return mysqli_num_fields($query);
+	} else {
+		return mysql_num_fields($query);
+	}
 }
 
 function db_free_result($query) {
-	return mysql_free_result($query);
+	global $db_type;
+	if($db_type == 'mysqli') {
+		return mysqli_free_result($query);
+	} else {
+		return mysql_free_result($query);
+	}
 }
 
 function db_insert_id() {
-	$id = mysql_insert_id();
+	global $db_type;
+	if($db_type == 'mysqli') {
+		$id = mysqli_insert_id();
+	} else {
+		$id = mysql_insert_id();
+	}
 	return $id;
 }
 
 function db_fetch_row($query) {
-	$query = mysql_fetch_row($query);
+	global $db_type;
+	if($db_type == 'mysqli') {
+		$query = mysqli_fetch_row($query);
+	} else {
+		$query = mysql_fetch_row($query);
+	}
 	return $query;
 }
 
 function db_close() {
-	mysql_close();
+	global $db_type;
+	if($db_type == 'mysqli') {
+		mysqli_close();
+	} else {
+		mysql_close();
+	}
+}
+
+function db_field_name($result, $field_offset) {
+	if($db_type == 'mysqli') {
+        return mysqli_field_name($result, $field_offset);
+	} else {
+        return mysql_field_name($result, $field_offset);
+	}
+}
+
+function db_get_server_info() {
+	if($db_type == 'mysqli') {
+        return mysqli_get_server_info();
+	} else {
+        return mysql_get_server_info();
+	}
 }
 
 function db_halt($message = '', $sql = '') {
-global $db_prefix, $config;
-$timestamp = time();
-$errmsg = '';
+    global $db_prefix, $config;
+    $timestamp = time();
+    $errmsg = '';
 
-$dberror = db_error();
-$dberrno = db_errno();
-$dberror=str_replace($db_prefix, '***', $dberror);
-$sql=str_replace($db_prefix, '***', $sql);
+    $dberror = db_error();
+    $dberrno = db_errno();
+    $dberror=str_replace($db_prefix, '***', $dberror);
+    $sql=str_replace($db_prefix, '***', $sql);
 
 
-$errmsg = "<b>Bo-Blog Database System Tips</b>: $message\n\n";
+    $errmsg = "<b>Bo-Blog Database System Tips</b>: $message\n\n";
 /*vot*/ $errmsg .= "<b>Time</b>: ".gmdate("Y-m-d H:i:s", $timestamp + ($config['timezone'] * 3600))."\n";
 /*vot*/ $errmsg .= "<b>Script</b>: ".$_SERVER['PHP_SELF']."\n\n";
-if($sql) {
-	$errmsg .= "<b>SQL</b>: ".htmlspecialchars($sql)."\n";
-}
-$errmsg .= "<b>Error</b>:  $dberror\n";
-$errmsg .= "<b>Errno.</b>:  $dberrno";
+    if($sql) {
+    	$errmsg .= "<b>SQL</b>: ".htmlspecialchars($sql)."\n";
+    }
+    $errmsg .= "<b>Error</b>: $dberror\n";
+    $errmsg .= "<b>Errno.</b>: $dberrno";
 
-@header("Content-Type: text/html; charset=utf-8");
-echo "</table></table></table></table></table>\n";
-echo "<p style=\"font-family: Verdana, Tahoma; font-size: 11px; background: #FFFFFF;\">";
-echo nl2br($errmsg);
-	
-echo '</p>';
-exit;
+    @header("Content-Type: text/html; charset=utf-8");
+    echo "</table></table></table></table></table>\n";
+    echo "<p style=\"font-family: Verdana, Tahoma; font-size: 11px; background: #FFFFFF;\">";
+    echo nl2br($errmsg);
+    echo '</p>';
+
+    backtrace();
+
+    exit;
 }
